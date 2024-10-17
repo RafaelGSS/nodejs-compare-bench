a2e4a192e6fecf5187cbbf930debc554f4a1c52a
RafaelGSS <rafael.nunu@hotmail.com>
2024-10-17 15:46:34 -0300

    feat: print env details

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const path = require('node:path');
+const { platform, arch, cpus, totalmem } = require('node:os');
 const Piscina = require('piscina');
 
 const USE_CASES = ['copied-function', 'empty-function'];
@@ -10,16 +11,24 @@ const piscina = new Piscina({
 
 const halfScreen = process.stdout.columns / 2;
 
+const machineInfo = `${platform()} ${arch()} | ${cpus().length} vCPUs | ${(totalmem() / (1024 ** 3)).toFixed(1)}GB Mem`
+
+console.log(`\n
+* __Machine:__ ${machineInfo}
+* __Node:__ \`${process.version}\`
+* __Run:__ ${new Date()}
+`)
+
 async function main() {
   for (const c of USE_CASES) {
     console.log(`${'_'.repeat(halfScreen / 2)}${c}${'_'.repeat(halfScreen / 2)}`);
     for (const bench of BENCHMARKS) {
-      console.info(`Running... ${bench}`);
+      console.log(`Running... ${bench}`);
       await piscina.run({
         module: bench,
         useCase: c,
       });
-      console.info('-'.repeat(halfScreen));
+      console.log('-'.repeat(halfScreen));
     }
   }
 }
